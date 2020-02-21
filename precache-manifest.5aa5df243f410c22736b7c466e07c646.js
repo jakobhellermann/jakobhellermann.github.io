8ba479691416fc11d19e816dcfeeb03551bec09d
Jakob Hellermann <jakob.hellermann@protonmail.com>
2020-02-21 20:10:50 +0100

    Initial commit

---
+++ precache-manifest.5aa5df243f410c22736b7c466e07c646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bc50a3a8bae6be69e7184658bef3de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afd68d89dac86410388e",
+    "url": "/static/js/2.b84e8783.chunk.js"
+  },
+  {
+    "revision": "13e664f77c29f722aa40defc01fa182a",
+    "url": "/static/js/2.b84e8783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36e493a3f37dd3b349e",
+    "url": "/static/js/main.d0341c10.chunk.js"
+  },
+  {
+    "revision": "a1292272db6b9e2c540a",
+    "url": "/static/js/runtime-main.0e679891.js"
+  }
+]);
